ab3f2c9aff4607c7d428537c9594336aad479ab5
Hulbert Zeng <hulbert2013@gmail.com>
2021-02-15 18:32:58 -0800

    Completed part 1

---
+++ turnin/hzeng012_lab9_part4.c
@@ -0,0 +1,25 @@
+ /* Author: Hulebrt Zeng
+ * Partner(s) Name (if applicable):  
+ * Lab Section: 021
+ * Assignment: Lab #9  Exercise #1
+ * Exercise Description: [optional - include for your own benefit]
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code, is my own original work.
+ *
+ *  Demo Link: Youtube URL>
+ */ 
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
